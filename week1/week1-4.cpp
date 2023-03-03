5f7751a611bad47e20142470541808d11752b189
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-03-03 08:34:57 +0800

    week01

---
+++ week1/week1-4.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char line[40];
+    int total=0;
+    scanf("%s",line);
+    int ans=0;
+    int N = strlen(line);
+    for(int i=0;i<N;i++)
+    {
+        if(line[i]=='2');
+        ans++;
+    }
+    printf("%d\n",ans);
+    total += ans;
+
+}
